17b3fa478b1db99dea792c404b064b29c486894c
venelinpetrov <venelin.petrov@hyperscience.com>
2020-04-29 16:39:39 +0300

    Fix the case where you perform actions while you are back in history

--- src/hooks/useHistory.js
+++ src/hooks/useHistory.js
@@ -5,9 +5,16 @@ export const useHistory = (initialState, size = 5) => {
   const [step, setStep] = useState(0);
 
   const getCurrent = () => {
-    return history[step]
+    return history[step];
   };
   const concat = x => setHistory((prevHistory) => {
+    // Making a history entry from point back in time should add
+    // the entry after this point and discard the "future" entries
+    if (step < history.length - 1) {
+      setStep(step + 1);
+      return prevHistory.slice(0, step + 1).concat(x);
+    }
+    // Keep the history at maximum size of 'size'
     if (history.length >= size) {
       return prevHistory.slice(1).concat(x);
     }
